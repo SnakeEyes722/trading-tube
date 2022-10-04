45a6c578b9c84736d8bddbc9e644075081e3f992
SnakeEyes722 <110727766+SnakeEyes722@users.noreply.github.com>
2022-10-04 12:34:10 +0500

    Update DepositController.php

--- app/Http/Controllers/DepositController.php
+++ app/Http/Controllers/DepositController.php
@@ -11,12 +11,17 @@ class DepositController extends Controller
 {
     function AddDeposit(Request $req){
 
-        $deposit = new Deposit;
-        $deposit -> user_id = $req->user_id;
-        $deposit -> amount = $req->amount;
-        $deposit -> verified = $req->verified??'false';
+         $deposit = new Deposit;
+         $deposit -> payer_id = $req->payer_id;
+         $deposit -> account_type = $req->account_type;
+         $deposit -> account_title = $req->account_title;
+         $deposit -> account_no = $req->account_no;
+         $deposit -> proof_image = $req->proof_image;
 
-        $result =  $deposit -> save();
+         $deposit -> amount = $req->amount;
+         $deposit -> verified = $req->verified??'false';
+
+         $result =  $deposit -> save();
     
 
         if($result){
@@ -35,56 +40,5 @@ class DepositController extends Controller
 
     }
 
-    function UpdateDeposit(Request $req,$user_id){
-
-
-        $deposit =Deposit::where('user_id',$user_id)->first();
-       
-        $deposit -> verified = $req->verified??'false';
-
-        //$deposit = Deposit::where('user_id', '=',  $req->user_id)->get();
-
-       $data =  DB::table('deposits')
-        
-             ->select('balance')
-            ->where('user_id',$user_id)
-            ->first();
-
-            DB::table('balances')->insert($data)([
-
-                        'amount' => $data->balance,
-        
-                    ]);
-        
-        $result =  $deposit -> update($req->all());
     
-        if($result){
-
-            return response([
-                "status" => '200',
-                "message" =>'data has been updated'
-            ]);
-        }
-        else{
-        return response([
-                "message" =>'Operation failed'
-            ]);
-        }
-
-
-        // $deposit = new Deposit;
-
-        // $deposit = Deposit::where('user_id', '=',  $req->user_id)->get();
-        // $count = $deposit->count();
-
-        // DB::table('balances')
-        // ->where('user_id', $req->user_id)
-        // ->increment('total_deposit', $req->total_deposit && 'balance');
-
-        // DB::table('deposits')
-        // ->where('user_id', $req->user_id)
-        // ->update('verifies', $req->verified);
-
-      
-    }
 }
